466f61ad72cf85e228c9fc85687a8819376e5ac3
sxis <sxis@localhost.localdomain>
2016-01-28 02:02:54 -0500

    在trees中增加了对第二章测试数据的加载和处理。

--- machinelearninginaction/Ch03/trees.py
+++ machinelearninginaction/Ch03/trees.py
@@ -5,6 +5,7 @@ Decision Tree Source Code for Machine Learning in Action Ch. 3
 '''
 from math import log
 import operator
+import numpy as np
 
 def createDataSet():
     dataSet = [[1, 1, 'yes'],
@@ -16,10 +17,35 @@ def createDataSet():
     #change to discrete values
     return dataSet, labels
 
+def file2List(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())         #get the number of lines in the file
+    returnMat = np.zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return   
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+	line = line.strip()
+	line = line.strip('/n')
+	listFromLine = line.split('\t')
+	returnMat[index,:] = listFromLine[0:3]
+	classLabelVector.append((listFromLine[-1]))
+	index += 1
+
+    DataArr = np.asarray(returnMat)
+    DataList = []
+    
+    for eA in DataArr:
+        DataOne = []
+        for el in eA:
+            DataOne.append(el)
+        DataList.append(DataOne)
+    return DataList,classLabelVector
+
+
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     print(numEntries)
-    print('create dic')
     labelCounts = {}
     for featVec in dataSet: #the the number of unique elements and their occurance
         currentLabel = featVec[-1]
@@ -41,6 +67,15 @@ def splitDataSet(dataSet, axis, value):
             reducedFeatVec.extend(featVec[axis+1:])
             retDataSet.append(reducedFeatVec)
     return retDataSet
+
+def splitDataSet2(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] >= value:
+            reducedFeatVec = featVec[:axis]     #chop out axis used for splitting
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
     
 def chooseBestFeatureToSplit(dataSet):
     numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
@@ -52,6 +87,24 @@ def chooseBestFeatureToSplit(dataSet):
         newEntropy = 0.0
         for value in uniqueVals:
             subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)     
+        infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
+        if (infoGain > bestInfoGain):       #compare this to the best gain so far
+            bestInfoGain = infoGain         #if better than current best, set to best
+            bestFeature = i
+    return bestFeature                      #returns an integer
+
+def chooseBestFeatureToSplit2(dataSet):
+    numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0; bestFeature = -1
+    for i in range(numFeatures):        #iterate over all the features
+        featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
+        uniqueVals = set(featList)       #get a set of unique values
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet2(dataSet, i, value)
             prob = len(subDataSet)/float(len(dataSet))
             newEntropy += prob * calcShannonEnt(subDataSet)     
         infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
@@ -83,7 +136,25 @@ def createTree(dataSet,labels):
     for value in uniqueVals:
         subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels)
-    return myTree                            
+    return myTree  
+
+def createTree2(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList): 
+        return classList[0]#stop splitting when all of the classes are equal
+
+    if len(dataSet[0]) == 1: #stop splitting when there are no more features in dataSet
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit2(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
+        myTree[bestFeatLabel][value] = createTree2(splitDataSet(dataSet, bestFeat, value),subLabels)
+    return myTree                           
     
 def classify(inputTree,featLabels,testVec):
     firstStr = inputTree.keys()[0]
