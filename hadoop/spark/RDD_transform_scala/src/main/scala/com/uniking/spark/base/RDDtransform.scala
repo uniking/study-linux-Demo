426dcdf083b33e75487f54924ad30729e89675c8
uniking <578246786@qq.com>
2016-04-12 06:30:42 -0400

    add rdd transform test

---
+++ hadoop/spark/RDD_transform_scala/src/main/scala/com/uniking/spark/base/RDDtransform.scala
@@ -0,0 +1,25 @@
+package com.uniking.spark.base
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+/**
+ * 统计字符出现次数
+ */
+object RDDtransform{
+  def main(args: Array[String]) {
+    if (args.length < 1) {
+      System.err.println("Usage: <file>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf()
+    val sc = new SparkContext(conf)
+    val line = sc.textFile(args(0))
+
+    line.flatMap(_.split("\t")).map((_, 1)).reduceByKey(_+_).collect.foreach(println)
+    
+    sc.stop
+  }
+}
