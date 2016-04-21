d016687c583670fa9f216472716cdd3eac3a1e36
uniking <578246786@qq.com>
2016-04-21 03:05:31 -0400

    add json test

--- json/cpp/main.cpp
+++ json/cpp/main.cpp
@@ -1,4 +1,4 @@
-#include "/home/sxis/workspace/NDLP50000/discover/Dll/jeson/include/json.h"
+#include "json/json.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -7,23 +7,47 @@
 #include <sstream>
 using namespace std;
 
+#define FILE_NAME "test.json"
+
+// {"0":{"sun0":10},"1":[{"sun":12},{"sun":11}],"int":100,"string":"test"}
+
 class jsonTest
 {
 private:
 	void usejson(Json::Value json_object);
+   void json_value();
+   int JsonWrite(Json::Value& root, char* filepath);
 public:
-	void sendjson();
+	void readjson();
+   void savejson();
 };
 
 void jsonTest::usejson(Json::Value json_object)
 {
-	int classid = (json_object["classid"]).asInt();//各种类型的id
-	printf("passJson %d\n", classid);
+	int classid = (json_object["int"]).asInt();//各种类型的id
+    string test = (json_object["test"]).asString();//各种类型的id
+    cout<<classid<<" "<<test<<endl;
+
+    Json::Value sun0 = json_object["0"];
+    int sun0_9 = (sun0["sun0"]).asInt();
+    cout<<sun0_9<<endl;
+
+    Json::Value sun_l(Json::arrayValue);
+    sun_l = json_object["1"];
+
+    Json::Value::iterator tmp = sun_l.begin();
+    while(tmp != sun_l.end())
+    {
+        int i = (*tmp)["sun"].asInt();
+        cout<<i<<endl;
+        tmp++;
+    }
+    
 }
 
-void jsonTest::sendjson()
+void jsonTest::readjson()
 {
-	ifstream infile("./phone.re");
+	ifstream infile(FILE_NAME);
 	string sContent;
 	ostringstream os;
 	os << infile.rdbuf();
@@ -38,10 +62,64 @@ void jsonTest::sendjson()
 
 	usejson(json_object);
 }
+
+int jsonTest::JsonWrite(Json::Value& root, char* filepath)
+{
+	ofstream outfile(filepath);
+	ostringstream os;
+	Json::FastWriter fast_writer;
+	string temp = fast_writer.write(root);
+	outfile << temp;
+	
+	return 0;
+}
+
+void jsonTest::savejson()
+{
+    Json::Value root;
+
+    root["string"] = Json::Value("test");
+    root["int"] = Json::Value(100);
+    
+    Json::Value sun0;
+    sun0["sun0"] = Json::Value(10);
+
+/*
+    Json::Value sun1;
+    sun1["sun1"] = Json::Value(11);
+
+    Json::Value sun2;
+    sun2["sun2"] = Json::Value(12);
+*/
+
+/*
+    Json::Value sun1[2];
+    sun1[0] = Json::Value(11);
+    sun1[1] = Json::Value(12);
+*/
+
+
+    Json::Value sun1;
+    Json::Value sun2;
+    Json::Value sun3(Json::arrayValue);
+    sun1["sun"] = Json::Value(12);
+    sun2["sun"] = Json::Value(11);
+    sun3[0] = Json::Value(sun1);
+    sun3[1] = Json::Value(sun2);
+
+
+    root["0"] = Json::Value(sun0);
+
+
+    root["1"] = Json::Value(sun3);
+    JsonWrite(root, "test.json");
+}
+
 int main()
 {
 
 	jsonTest abc;
-	abc.sendjson();
+    abc.savejson();
+	abc.readjson();   
 	return 0;
 }
