5e7d5c63f068ac305f4e781f45a865ba844bc248
uniking <578246786@qq.com>
2016-10-14 18:13:03 +0800

    process test

---
+++ process/process.cpp
@@ -0,0 +1,38 @@
+//fork wait waitpid signal pause kill raise exec exit
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#include <iostream>
+using namespace std;
+
+void say_hello(char* mark, int times)
+{
+	int i=0;
+	while(i < times)
+	{
+		sleep(1);
+		cout<<mark<<" "<<i<<endl;
+		i++;
+	}
+
+	exit(1);
+}
+
+int main()
+{
+	pid_t pid = fork();
+	if(pid == 0)
+	{//child
+		say_hello("child", 5);
+		exit(3);
+	}
+	else
+	{//parent
+		int stat_loc;
+		pid_t wip = wait(&stat_loc);
+		cout<<"wip:"<<wip<<" stat_loc:"<<stat_loc<<endl;
+		cout<<"child pid:"<<pid<<endl;
+	}
+}
